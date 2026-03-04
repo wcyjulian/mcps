c3af8cca52aa4ce0f5f7664062882126ca4ffa7b
Julian Wang <wcyjulian@gmail.com>
2026-03-04 21:00:47 +0100

    aa

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from mcps!")
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get("/showtut1", response_class=HTMLResponse)
+def showtut1():
+    """REST API endpoint that returns HTML text 'Hi there'"""
+    return "Hi there"
 
 
 if __name__ == "__main__":
-    main()
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
