773501126f9ca48928e88ba673dfeada8b63df9f
Julian Lin <wcyjulian@gmail.com>
2026-04-02 17:26:22 +0200

    Fix indentation in showtut1 function

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ app = FastAPI()
 @app.get("/showtut1", response_class=HTMLResponse)
 def showtut1():
     """REST API endpoint that returns HTML text 'Hi there'"""
+    "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"
     return "Hi there"
 
 
